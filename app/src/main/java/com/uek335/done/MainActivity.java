e6854eac96ce62e496f2957e1b9898c37dfe9743
DenisWilhelm <2denisw@gmail.com>
2020-02-03 16:42:16 +0100

    Added first view

--- app/src/main/java/com/uek335/done/MainActivity.java
+++ app/src/main/java/com/uek335/done/MainActivity.java
@@ -1,9 +1,11 @@
 package com.uek335.done;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.uek335.done.activity.CreateTaskView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -21,12 +23,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addAction = findViewById(R.id.addTask);
+        addAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent showCreateTaskView = new Intent(MainActivity.this, CreateTaskView.class);
+                // parameters ==> showCreateTaskView.putExtra("key", param);
+                MainActivity.this.startActivity(showCreateTaskView);
             }
         });
     }
---
+++ app/src/main/java/com/uek335/done/activity/CreateTaskView.java
@@ -0,0 +1,16 @@
+package com.uek335.done.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uek335.done.R;
+
+public class CreateTaskView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task_view);
+    }
+}
